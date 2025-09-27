d2a6cc469099d50e38f19d88cc83360c75e1b8cc
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-27 21:15:51 +0100

    Updated firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,21 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-
-
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDrNiFLm_jwAS6pRstetAOo3KOWkzmf8y0",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "facture-bc21d.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "facture-bc21d",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "facture-bc21d.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "15503201564",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:15503201564:web:8f61217b6e35dfbd2ad6d9",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-581B5HXX2H"
+  apiKey: "AIzaSyDrNiFLm_jwAS6pRstetAOo3KOWkzmf8y0",
+  authDomain: "facture-bc21d.firebaseapp.com",
+  projectId: "facture-bc21d",
+  storageBucket: "facture-bc21d.firebasestorage.app",
+  messagingSenderId: "15503201564",
+  appId: "1:15503201564:web:8f61217b6e35dfbd2ad6d9",
+  measurementId: "G-581B5HXX2H"
 };
-
-
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
