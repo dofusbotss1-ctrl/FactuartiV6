2e74b679227f950c0f6e7111bcb8657aae44cafd
Abdel20255558 <abdelidrissi25@gmail.com>
2025-09-29 02:30:00 +0100

    Updated HRManagement.tsx

--- src/components/hr/HRManagement.tsx
+++ src/components/hr/HRManagement.tsx
@@ -286,13 +286,7 @@ export default function HRManagement() {
               ))}
             </select>
           </div>
-          <button
-            onClick={exportHRReport}
-            className="inline-flex items-center space-x-2 bg-gradient-to-r from-blue-600 to-indigo-600 hover:from-blue-700 hover:to-indigo-700 text-white px-4 py-2 rounded-lg transition-all duration-200"
-          >
-            <Download className="w-4 h-4" />
-            <span>Export PDF</span>
-          </button>
+         
         </div>
       </div>
 
